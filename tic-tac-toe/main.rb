0f9bb628aa646296350d0bd5738277f91f00164c
Cesar Quintana <c.evaristo.quintana@gmail.com>
2026-03-28 22:32:56 -0600

    working on game

--- tic-tac-toe/main.rb
+++ tic-tac-toe/main.rb
@@ -1,7 +1,12 @@
 require_relative 'lib/display'
+require_relative 'lib/game'
 
 display = Display.new
 
 board = ['X', 'O', 'X', ' ', 'X', ' ', 'O', ' ', 'O']
 
 display.show_board(board)
+
+game = Game.new
+
+game.make_move('A1', 'X')
