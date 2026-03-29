82481c498b71bff2e97529c18070994c3b095af9
Cesar Quintana <c.evaristo.quintana@gmail.com>
2026-03-28 22:08:42 -0600

    display object

--- tic-tac-toe/lib/display.rb
+++ tic-tac-toe/lib/display.rb
@@ -3,10 +3,11 @@
 # For displaying the tic-tac-toe board
 class Display
   def show_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts '---+---+---'
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts '---+---+---'
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts '  A | B | C '
+    puts "1 #{board[0]} | #{board[1]} | #{board[2]} "
+    puts ' ---+---+---'
+    puts "2 #{board[3]} | #{board[4]} | #{board[5]} "
+    puts ' ---+---+---'
+    puts "3 #{board[6]} | #{board[7]} | #{board[8]} "
   end
 end
