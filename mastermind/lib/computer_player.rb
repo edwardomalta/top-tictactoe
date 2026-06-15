ebdbb752ca8cad5a8f0845eb0120fb60de65f8ec
Cesar Quintana <c.evaristo.quintana@gmail.com>
2026-06-14 23:24:17 -0600

    including combinations

--- mastermind/lib/computer_player.rb
+++ mastermind/lib/computer_player.rb
@@ -2,6 +2,7 @@
 class ComputerPlayer
   def initialize(colors)
     @colors = colors
+    @combinations = gen_combinations(Array.new(4, 0), 0, 6)
   end
 
   def gen_code
@@ -13,5 +14,20 @@ class ComputerPlayer
   def feedback(array)
     puts array
     puts "mames que voy a hacer con esta data?"
+  end
+
+  def gen_combinations(current, position, max)
+    return [current.dup] if position == current.length
+
+    result = []
+    (1..max).each do |value|
+      current[position] = value
+      result += gen_combinations(current, position + 1, max)
+    end
+    result
+  end
+
+  def pensar
+    puts "Pensamiento tengo #{@combinations.length} combinaciones posibles... ¿cómo reduzco el problema?"
   end
 end
--- mastermind/lib/game.rb
+++ mastermind/lib/game.rb
@@ -131,6 +131,7 @@ class Game
   # May be this is going to be in other class:
   def computer_guesser_v1
     # gen_code.map { |x| x.to_s }
+    @computer_player.pensar
     @computer_player.gen_code
   end
 
