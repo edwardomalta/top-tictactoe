c73f3adc5a425f405dedc02892a0cffc02594e20
Cesar Quintana <c.evaristo.quintana@gmail.com>
2026-07-17 00:13:02 -0600

    cleaning even more

--- mastermind/lib/game.rb
+++ mastermind/lib/game.rb
@@ -125,7 +125,8 @@ class Game
   end
 
   def user_code
-    puts @colors.to_yaml if @try_index == MAX_NUMBER_OF_TRIES
+    # Esto de abajo parece que no será necesario por ahora
+    # puts @colors.to_yaml if @try_index == MAX_NUMBER_OF_TRIES
     print "Selecciona un codigo de cuatro colores: "
     result = gets
     result.split.map(&:to_i)
