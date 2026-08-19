a974006d5edea108cabc2b74ebbf2f1f5acf4fb6
Cesar Quintana <c.evaristo.quintana@gmail.com>
2026-08-19 01:16:27 -0600

    unit test for make_move

--- tic-tac-toe/spec/game_spec.rb
+++ tic-tac-toe/spec/game_spec.rb
@@ -9,7 +9,8 @@ describe Game do
     context "receives correct movement" do
       before do
         allow(game_start).to receive(:make_move)
-        allow(game_start).to receive(:status)
+        allow(game_start).to receive(:get_player_move)
+allow(game_start).to receive(:status)
         game_start.instance_variable_set(:@display, display_double)
       end
       it "calls #get_player_move by 3" do
@@ -18,21 +19,25 @@ describe Game do
         game_start.start
       end
 
-      xit "increases @counter by one" do
-      end
-
-      xit "calls @display.show_board" do
-      end
-
       context "when there is a winner" do
-        xit "ends with congrats to winner" do
+        it "ends with congrats to winner" do
+          allow(game_start).to receive(:game_over?).and_return(false, false, true)
+          expect(game_start).to receive(:announce_winner).once
+          game_start.start
         end
       end
 
       context "when there is no way to win" do
-        xit "ends with tables message" do
+        it "ends with tables message" do
+          allow(game_start).to receive(:game_over?).and_return(true)
+          expect(game_start).to receive(:announce_winner).once
+          game_start.start
         end
       end
     end
+  end
+
+  describe "#make_move" do
+    
   end
 end
